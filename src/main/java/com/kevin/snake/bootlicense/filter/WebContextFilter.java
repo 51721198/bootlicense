2d27cf63e0b4f4f0cd04956f8ddab26b66ba875d
liudun <liudun@drilver.com>
2018-03-17 14:10:41 +0800

    fix

--- src/main/java/com/kevin/snake/bootlicense/filter/WebContextFilter.java
+++ src/main/java/com/kevin/snake/bootlicense/filter/WebContextFilter.java
@@ -1,11 +1,14 @@
 package com.kevin.snake.bootlicense.filter;
 
+import org.springframework.core.annotation.Order;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -15,6 +18,8 @@ import java.io.IOException;
  * @author Liu.Dun
  *         用于管理webcontext对象的生命周期
  */
+@Order(2)
+@WebFilter(filterName = "WebContextFilter", urlPatterns = "/*")
 public class WebContextFilter implements Filter {
 
     @Override
