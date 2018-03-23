689c6eda0470bcc27cd932d6a6873ef2674eb6b2
liudun <liudun@drilver.com>
2018-03-23 18:22:55 +0800

    fix

---
+++ src/main/java/com/kevin/snake/bootlicense/controller/RedisController.java
@@ -0,0 +1,22 @@
+package com.kevin.snake.bootlicense.controller;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping(value = "redis")
+public class RedisController {
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @RequestMapping(value = "script")
+    public String excuteScript(){
+        return "";
+    }
+
+
+}
