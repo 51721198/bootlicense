aafcc518da6b76bbaa1297a3493f59992a835f2d
liudun <liudun@drilver.com>
2018-05-13 22:07:17 +0800

    fix

--- src/main/java/com/kevin/snake/bootlicense/BootlicenseApplication.java
+++ src/main/java/com/kevin/snake/bootlicense/BootlicenseApplication.java
@@ -3,11 +3,10 @@ package com.kevin.snake.bootlicense;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @ServletComponentScan
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class BootlicenseApplication {
 
 	public static void main(String[] args) {
